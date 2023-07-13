e694f0ac4c3253bcbb54969fe725753a79dc3e3f
Metarails <SamiSantamaa87@gmail.com>
2023-07-13 09:41:10 +0300

    4.1 blogilista, step1 DONE

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -13,7 +14,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -28,6 +29,9 @@ app.get('/api/blogs', (request, response) => {
 })
 
 app.post('/api/blogs', (request, response) => {
+
+    console.log("requwsst body: ", request.body)
+  
   const blog = new Blog(request.body)
 
   blog
