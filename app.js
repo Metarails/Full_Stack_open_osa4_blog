20be58f81c258357dfb7aadcd4a37d4996182231
Metarails <SamiSantamaa87@gmail.com>
2023-07-13 11:11:28 +0300

    4.2 blogilista, step2 DONE

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const config = require("./utils/config")
+const blogsRouter = require("./controllers/blogs")
+
+mongoose.connect(config.mongoUrl)
+    .then(() => {
+        console.log("connected to MongoDB")
+    })
+    .catch((error) => {
+        console.log("error connection to MongoDB:", error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/blogs", blogsRouter)
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+
+let mongoUrl = process.env.MONGODB_URI
+let PORT = process.env.PORT || 3003
+
+module.exports = {
+    mongoUrl,
+    PORT
+}
